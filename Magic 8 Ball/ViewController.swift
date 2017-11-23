a07ce249a99cb1b430dc26f7d816f90af07f4afb
Xina-Torres <xina.torres@gmail.com>
2017-11-23 16:18:00 +0800

    first commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Maria Xina Rae Torres on 23/11/2017.
+//  Copyright © 2017 Maria Xina Rae Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
